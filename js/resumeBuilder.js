253491e35c66da690ce1df120343bedc8cdf82e6
SiyuanWelch <mrs.swanwelch@gmail.com>
2015-10-14 16:53:45 -0700

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,3 +2,9 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
  $("#main").append([siyuan]);
+ var awesomeThoughts =
+ "my name is Siyuan and I am AWESOME!";
+ 
+ var awesomeThoughts=
+  awesomeThoughts.replace ("AWESOME","Fun");
+  ("#main").append(funThoughts);
